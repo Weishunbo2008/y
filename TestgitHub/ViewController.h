2e2ac18ef1a6f8389ab3fd12fd567d02219ddadc
Weishunbo2008 <WeishunboDmac@weishunbodeMacBook-Pro.local>
2015-11-08 15:44:55 +0800

    建立Base

---
+++ TestgitHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestgitHub
+//
+//  Created by 韦顺波 on 15/11/8.
+//  Copyright © 2015年 韦顺波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestgitHub/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestgitHub
+//
+//  Created by 韦顺波 on 15/11/8.
+//  Copyright © 2015年 韦顺波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
